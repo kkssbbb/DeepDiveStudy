452eb06a59545edb203e14394475aa6bf7e1b8d7
SeungBin <natty0337@gmail.com>
2022-11-15 23:54:15 +0900

    1weekStudy

---
+++ nattyKing/1week/test.js
@@ -0,0 +1,15 @@
+
+
+// function test(){
+//     var a = 10; 
+// }
+
+
+// console.log(typeof a);
+// console.log(typeof a ==="undefined"); 
+
+var a = {};
+//var a ;
+console.log(a);
+console.log(typeof a);
+
